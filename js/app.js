2d57d0bbeac07a51994d62d65468a2d7a46f9983
Vladimir Zecevic <zecevicvladimir1@gmail.com>
2020-12-29 16:20:03 +0100

    [ADD] - Homepage - Animations

--- js/app.js
+++ js/app.js
@@ -1,41 +1,5 @@
-/* #Tab Functionality
-  ======================================================= */
-const tabLinksGroup = document.querySelectorAll('.tab-links');
-tabLinksGroup.forEach((tabLinkGroup) => {
-  // Display only active tab panes
-  tabLinkGroup.querySelectorAll('.tab-link').forEach((tabLink) => {
-    if (tabLink.classList.contains('active')) {
-      document.getElementById(tabLink.getAttribute("href")).classList.add('active');
-    }
-  });
-
-  // Tab-link event listener
-  tabLinkGroup.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (e.target.closest('.tab-link')) {
-      if (!e.target.closest('.tab-link').classList.contains('active')) {
-        // Remove all active classes from tab-links
-        tabLinkGroup.querySelectorAll('.tab-link').forEach((tabLink) => {
-          tabLink.classList.remove('active');
-        });
-        // Remove all active classes from tab-panes
-        document.getElementById(e.target.getAttribute("href"))
-        .closest('.tab-content')
-        .querySelectorAll('.tab-pane').forEach((tabPane) => {
-          tabPane.classList.remove('active');
-        });
-
-        // Add active class to the clicked tab-link
-        e.target.closest('.tab-link').classList.add('active');
-        // Add active class to the corresponding tab-pane
-        document.getElementById(e.target.getAttribute("href")).classList.add('active');
-      }
-    }
-  });
-});
-
 /* #Header Scroll
-  ======================================================= */
+    ======================================================= */
 const header = document.querySelector('.header');
 window.addEventListener('scroll', () => {
   if (window.scrollY > 50) {
@@ -49,125 +13,6 @@ if (window.scrollY > 50) {
 } else {
   header.classList.remove('header-dark');
 }
-
-/* #Appartments Sliders
-  ======================================================= */
-new Swiper('.the-appartments .swiper-container', {
-  slidesPerView: 1.22,
-  breakpoints: {
-    0: {
-      slidesPerView: 1.1
-    },
-    1024: {
-      slidesPerView: 1.22
-    },
-  }
-});
-
-document.querySelectorAll('.typical-appartments .swiper-container').forEach((slider) => {
-  new Swiper(slider, {
-    allowTouchMove: false,
-    slidesPerView: 5,
-    slidesPerColumn: 2,
-    slidesPerGroup: 5,
-    slidesPerColumnFill: 'row',
-    direction: 'horizontal',
-    uniqueNavElements: true,
-    breakpoints: {
-      0: {
-        allowTouchMove: true,
-        slidesPerView: 2,
-        slidesPerColumn: 2,
-        slidesPerGroup: 2,
-      },
-      1024: {
-        allowTouchMove: false,
-        slidesPerView: 5,
-        slidesPerColumn: 2,
-        slidesPerGroup: 5,
-      },
-    },
-    pagination: {
-      el: slider.closest('.appartment-types').querySelector('.swiper-pagination'),
-    },
-    navigation: {
-      nextEl: slider.closest('.appartment-types').querySelector('.swiper-button-next'),
-      prevEl: slider.closest('.appartment-types').querySelector('.swiper-button-prev'),
-    },
-  });
-});
-
-
-/* #Environment Sliders
-  ======================================================= */
-new Swiper('.environment-slider .swiper-container', {
-  slidesPerView: 1,
-  loop: true,
-  pagination: {
-    el: '.environment-slider .swiper-pagination',
-  },
-  navigation: {
-    nextEl: '.environment-slider .swiper-button-next',
-    prevEl: '.environment-slider .swiper-button-prev',
-  },
-});
-
-/* #Company Sliders
-  ======================================================= */
-new Swiper('.company-block-2 .swiper-container', {
-  allowTouchMove: false,
-  slidesPerView: 2,
-  slidesPerColumn: 2,
-  slidesPerGroup:2,
-  slidesPerColumnFill: 'row',
-  direction: 'horizontal',
-  pagination: {
-    el: '.company-block-2 .swiper-pagination',
-  },
-  navigation: {
-    nextEl: '.company-block-2 .swiper-button-next',
-    prevEl: '.company-block-2 .swiper-button-prev',
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 2.5,
-      slidesPerColumn: 1,
-      slidesPerGroup: 1,
-      allowTouchMove: true,
-      centeredSlides: true,
-      loop: true
-    },
-    1024: {
-      slidesPerView: 2,
-      slidesPerColumn: 2,
-      slidesPerGroup:2,
-      allowTouchMove: false
-    }
-  }
-});
-
-new Swiper('.company-block-3 .swiper-container', {
-  allowTouchMove: false,
-  slidesPerView: 3,
-  pagination: {
-    el: '.company-block-3 .swiper-pagination',
-  },
-  navigation: {
-    nextEl: '.company-block-3 .swiper-button-next',
-    prevEl: '.company-block-3 .swiper-button-prev',
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-      allowTouchMove: true,
-      loop: true
-    },
-    1024: {
-      slidesPerView: 3,
-      allowTouchMove: false,
-    }
-  }
-});
 
 /* #Hamburger Menu
   ======================================================= */
@@ -220,5 +65,174 @@ document.addEventListener('click', (e) => {
   if (modal.classList.contains('show') && e.target.closest('.modal-overlay')) {
     modal.classList.remove('show');
     body.classList.remove('no-scroll');
+  }
+});
+
+/* #Tab Functionality
+======================================================= */
+const tabLinksGroup = document.querySelectorAll('.tab-links');
+tabLinksGroup.forEach((tabLinkGroup) => {
+  // Display only active tab panes
+  tabLinkGroup.querySelectorAll('.tab-link').forEach((tabLink) => {
+    if (tabLink.classList.contains('active')) {
+      document.getElementById(tabLink.getAttribute("href")).classList.add('active');
+    }
+  });
+
+  // Tab-link event listener
+  tabLinkGroup.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (e.target.closest('.tab-link')) {
+      if (!e.target.closest('.tab-link').classList.contains('active')) {
+        // Remove all active classes from tab-links
+        tabLinkGroup.querySelectorAll('.tab-link').forEach((tabLink) => {
+          tabLink.classList.remove('active');
+        });
+        // Remove all active classes from tab-panes
+        document.getElementById(e.target.getAttribute("href"))
+          .closest('.tab-content')
+          .querySelectorAll('.tab-pane').forEach((tabPane) => {
+            tabPane.classList.remove('active');
+          });
+
+        // Add active class to the clicked tab-link
+        e.target.closest('.tab-link').classList.add('active');
+        // Add active class to the corresponding tab-pane
+        document.getElementById(e.target.getAttribute("href")).classList.add('active');
+      }
+    }
+  });
+});
+
+window.addEventListener('load', () => {
+  /* #Appartments Sliders
+    ======================================================= */
+  new Swiper('.the-appartments .swiper-container', {
+    slidesPerView: 1.22,
+    breakpoints: {
+      0: {
+        slidesPerView: 1.1
+      },
+      1024: {
+        slidesPerView: 1.22
+      },
+    }
+  });
+
+  document.querySelectorAll('.typical-appartments .swiper-container').forEach((slider) => {
+    new Swiper(slider, {
+      allowTouchMove: false,
+      slidesPerView: 5,
+      slidesPerColumn: 2,
+      slidesPerGroup: 5,
+      slidesPerColumnFill: 'row',
+      direction: 'horizontal',
+      uniqueNavElements: true,
+      breakpoints: {
+        0: {
+          allowTouchMove: true,
+          slidesPerView: 2,
+          slidesPerColumn: 2,
+          slidesPerGroup: 2,
+        },
+        1024: {
+          allowTouchMove: false,
+          slidesPerView: 5,
+          slidesPerColumn: 2,
+          slidesPerGroup: 5,
+        },
+      },
+      pagination: {
+        el: slider.closest('.appartment-types').querySelector('.swiper-pagination'),
+      },
+      navigation: {
+        nextEl: slider.closest('.appartment-types').querySelector('.swiper-button-next'),
+        prevEl: slider.closest('.appartment-types').querySelector('.swiper-button-prev'),
+      },
+    });
+  });
+
+  /* #Environment Sliders
+    ======================================================= */
+  new Swiper('.environment-slider .swiper-container', {
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+      el: '.environment-slider .swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.environment-slider .swiper-button-next',
+      prevEl: '.environment-slider .swiper-button-prev',
+    },
+  });
+
+  /* #Company Sliders
+    ======================================================= */
+  new Swiper('.company-block-2 .swiper-container', {
+    allowTouchMove: false,
+    slidesPerView: 2,
+    slidesPerColumn: 2,
+    slidesPerGroup: 2,
+    slidesPerColumnFill: 'row',
+    direction: 'horizontal',
+    pagination: {
+      el: '.company-block-2 .swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.company-block-2 .swiper-button-next',
+      prevEl: '.company-block-2 .swiper-button-prev',
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 2.5,
+        slidesPerColumn: 1,
+        slidesPerGroup: 1,
+        allowTouchMove: true,
+        centeredSlides: true,
+        loop: true
+      },
+      1024: {
+        slidesPerView: 2,
+        slidesPerColumn: 2,
+        slidesPerGroup: 2,
+        allowTouchMove: false
+      }
+    }
+  });
+
+  new Swiper('.company-block-3 .swiper-container', {
+    allowTouchMove: false,
+    slidesPerView: 3,
+    pagination: {
+      el: '.company-block-3 .swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.company-block-3 .swiper-button-next',
+      prevEl: '.company-block-3 .swiper-button-prev',
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        allowTouchMove: true,
+        loop: true
+      },
+      1024: {
+        slidesPerView: 3,
+        allowTouchMove: false,
+      }
+    }
+  });
+});
+
+/* #AOS Animations
+    ======================================================= */
+AOS.init({
+  startEvent: 'load',
+  once: true,
+  duration: 800,
+  offset: 300,
+  disable: function () {
+    var maxWidth = 1024;
+    return window.innerWidth < maxWidth;
   }
 });
